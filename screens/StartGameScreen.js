2622457452e0eded66f3a357114c78e06c83360c
amitkr7 <amit840901@gmail.com>
2023-08-19 00:40:38 +0530

    GameScreen

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { TextInput, View, StyleSheet, Alert } from 'react-native';
 import PrimaryButton from '../components/PrimaryButton';
 
-const StartGameScreen = () => {
+const StartGameScreen = ({ onPickNumber }) => {
   const [enteredNumber, setEnteredNumber] = useState('');
 
   const numberInputHandler = (enteredText) => {
@@ -21,6 +21,7 @@ const StartGameScreen = () => {
       ]);
       return;
     }
+    onPickNumber(enteredNumber);
     console.log('Valid Number');
   };
 
