a4c5fea85a06fdc667d18f85d0163083caceafa5
amitkr7 <amit840901@gmail.com>
2023-08-20 00:04:02 +0530

    code refactor, create and disaply random number

---
+++ constants/colors.js
@@ -0,0 +1,9 @@
+const Colors = {
+  primary500: '#72063c',
+  primary600: '#640233',
+  primary800: '#42063c',
+  accent500: '#ddb52f',
+  accent200: '#ffffff',
+};
+
+export default Colors;
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,8 +1,51 @@
-import React from 'react';
-import { Text } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-const GameScreen = () => {
-  return <Text>GameScreen</Text>;
+import Title from '../components/ui/Title';
+import NumberContainer from '../components/game/NumberContainer';
+
+const genearteRandomBetween = (min, max, exclude) => {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+  if (rndNum === exclude) {
+    return genearteRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+};
+
+const GameScreen = ({ userNumber }) => {
+  const initialGuess = genearteRandomBetween(1, 100, userNumber);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+  return (
+    <View style={styles.screen}>
+      <Title>Opponent's Guess</Title>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <View>
+        <Text>Higher or Lower?</Text>
+        {/* + - */}
+      </View>
+      <View>
+        <Text>LOG ROUNDS</Text>
+      </View>
+    </View>
+  );
 };
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#ddb52f',
+    textAlign: 'center',
+    borderWidth: 2,
+    borderColor: '#ddb52f',
+    padding: 12,
+  },
+});
