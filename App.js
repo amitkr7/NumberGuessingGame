1853159cab43923d6764f678e67df10d4ef5f356
amitkr7 <amit840901@gmail.com>
2023-08-27 00:23:16 +0530

    Styling Status Bar

--- App.js
+++ App.js
@@ -58,17 +58,20 @@ export default function App() {
   }
 
   return (
-    <LinearGradient
-      colors={[Colors.primary500, Colors.accent500]}
-      style={styles.rootScreen}>
-      <ImageBackground
-        source={require('./assets/images/background.png')}
-        resizeMode='cover'
-        style={styles.rootScreen}
-        imageStyle={styles.backgroundImage}>
-        <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
-      </ImageBackground>
-    </LinearGradient>
+    <>
+      <StatusBar style='light' />
+      <LinearGradient
+        colors={[Colors.primary500, Colors.accent500]}
+        style={styles.rootScreen}>
+        <ImageBackground
+          source={require('./assets/images/background.png')}
+          resizeMode='cover'
+          style={styles.rootScreen}
+          imageStyle={styles.backgroundImage}>
+          <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
+        </ImageBackground>
+      </LinearGradient>
+    </>
   );
 }
 
